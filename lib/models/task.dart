61c703dae0d4a2a25e4e453209182929ccb6fed4
JoshGarzagit config --global user.email jegarza98@gmail..com <jegarza98@gmail.com>
2019-11-30 14:16:24 -0600

    Add user, reminder, task, and event models

---
+++ lib/models/task.dart
@@ -0,0 +1,7 @@
+class Task {
+  int id;
+  String task;
+  DateTime created_at;
+
+  Task({this.id, this.task, this.created_at});
+}
--- lib/screens/welcome_page.dart
+++ lib/screens/welcome_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:productivist/models/user.dart';
 
 class WelcomePage extends StatefulWidget {
   @override
@@ -8,6 +9,50 @@ class WelcomePage extends StatefulWidget {
 class _WelcomePageState extends State<WelcomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      // appBar: AppBar(
+      //   backgroundColor: Color.fromRGBO(10, 51, 75, 1),
+      // ),
+      backgroundColor: Color.fromRGBO(10, 51, 75, 1),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(20, 70, 20, 10),
+                    child: Text(
+                      "Welcome ${currentUser.name}",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 46.0,
+                          fontFamily: "Calibre-Semibold",
+                          letterSpacing: 1.0),
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(25, 5, 10, 30),
+                    child: Text(
+                      "December 1, 2019",
+                      style: TextStyle(
+                          color: Colors.white30,
+                          fontSize: 20.0,
+                          fontFamily: "Calibre-Semibold",
+                          letterSpacing: .5),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
