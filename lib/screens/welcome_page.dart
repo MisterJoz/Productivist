390984ba9e707803be256fff1c3c8c6afd013d05
JoshGarzagit config --global user.email jegarza98@gmail..com <jegarza98@gmail.com>
2019-12-03 16:46:39 -0600

    Add calendar feature

--- lib/screens/welcome_page.dart
+++ lib/screens/welcome_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:productivist/models/user.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:productivist/screens/calendar_page.dart';
 
 class WelcomePage extends StatefulWidget {
   @override
@@ -10,10 +12,48 @@ class _WelcomePageState extends State<WelcomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   backgroundColor: Color.fromRGBO(10, 51, 75, 1),
-      // ),
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(10, 51, 75, 1),
+      ),
       backgroundColor: Color.fromRGBO(10, 51, 75, 1),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Drawer Header'),
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(10, 51, 75, 1),
+              ),
+            ),
+            ListTile(
+              title: Text('Item 1'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Calendar'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => CalendarPage()));
+                setState(() {});
+              },
+            ),
+          ],
+        ),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Container(
@@ -22,7 +62,7 @@ class _WelcomePageState extends State<WelcomePage> {
               Row(
                 children: <Widget>[
                   Padding(
-                    padding: EdgeInsets.fromLTRB(20, 70, 20, 10),
+                    padding: EdgeInsets.fromLTRB(20, 20, 20, 10),
                     child: Text(
                       "Welcome ${currentUser.name}",
                       style: TextStyle(
